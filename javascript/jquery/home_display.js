60e7f8e5129065645607e99a5f3117abd036fed7
Nicola Aitken <naitken22@gmail.com>
2014-08-05 16:19:22 +0100

    lists previous victory messages

---
+++ javascript/jquery/home_display.js
@@ -0,0 +1,12 @@
+	$(document).ready(function(){
+		var player1 = new Player("the player");
+		var player2 = new Player("the computer");
+		var game 		= new Game(player1, player2);
+	$('.choices img').on('click', function(){
+		player1.picks($(this).data('pick'));
+		player2.randomPick(this.randomPick);
+
+		$('#list').prepend("<li><h2>" + game.victoryStatement() + "</h2></li>");
+	})
+
+})
